5b7be97326130dcf5fc6889351292fc9c9d827f9
JackBorr <jbor@wp.pl>
2020-05-07 22:13:56 +0200

    Checkstyle

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,183 +1,191 @@
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.function.UnaryOperator;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Spliterator;
+import java.util.function.UnaryOperator;
 
 public class MyArrayList<T> implements List<T> {
 
-    private T[] tempTable;
-    private int size;
+  private T[] tempTable;
+  private int size;
 
-    public MyArrayList() {
-        tempTable =  (T[])new Object[0];
-        size = 0;
-    }
-    public MyArrayList(Collection<? extends T> c) {
+  public MyArrayList() {
+    tempTable = (T[]) new Object[0];
+    size = 0;
+  }
 
-            tempTable = (T[])c.toArray();
-            size = tempTable.length;
-    }
+  public MyArrayList(Collection<? extends T> c) {
 
-    public int size() {
-        return size;
-    }
+    tempTable = (T[]) c.toArray();
+    size = tempTable.length;
+  }
 
-    public boolean isEmpty() {
-        if(size==0) {
-            return true;
-        }
-        return false;
-    }
+  public int size() {
+    return size;
+  }
 
-    public boolean contains(final Object o) {
-        for(T element : tempTable) {
-            if(element.equals(o)) {
-                return true;
-            }
-        }
-        return false;
-    }
+  public boolean isEmpty() {
+    return size == 0;
+  }
 
-    public Iterator<T> iterator() {
-        return null;
-    }
-
-    public Object[] toArray() {
-        return Arrays.copyOf(tempTable, size);
-    }
-
-    public <T1> T1[] toArray(final T1[] a) {
-        return null;
-    }
-
-    public boolean add(final T t) {
-        add(size, t);
+  public boolean contains(final Object o) {
+    for (T element : tempTable) {
+      if (element.equals(o)) {
         return true;
+      }
+    }
+    return false;
+  }
+
+  public Iterator<T> iterator() {
+    return null;
+  }
+
+  public Object[] toArray() {
+    return Arrays.copyOf(tempTable, size);
+  }
+
+  public <T1> T1[] toArray(final T1[] a) {
+    return null;
+  }
+
+  public boolean add(final T t) {
+    add(size, t);
+    return true;
+  }
+
+  public void add(final int index, final T element) {
+    Object[] createTable = new Object[size + 1];
+    System.arraycopy(tempTable, 0, createTable, 0, index);
+    createTable[index] = element;
+    System.arraycopy(tempTable, index, createTable, index + 1, size - index);
+    size++;
+    tempTable = (T[]) createTable;
+  }
+
+  public boolean remove(final Object o) {
+    return false;
+  }
+
+  public T remove(final int index) {
+    return null;
+  }
+
+  public boolean containsAll(final Collection<?> c) {
+    return false;
+  }
+
+  public boolean addAll(final Collection<? extends T> c) {
+    return false;
+  }
+
+  public boolean addAll(final int index, final Collection<? extends T> c) {
+    return false;
+  }
+
+  public boolean removeAll(final Collection<?> c) {
+    return false;
+
+  }
+
+  public boolean retainAll(final Collection<?> c) {
+    return false;
+  }
+
+  public void replaceAll(final UnaryOperator<T> operator) {
+
+  }
+
+  public void sort(final Comparator<? super T> c) {
+
+  }
+
+  public void clear() {
+    tempTable = (T[]) new Object[0];
+    size = 0;
+  }
+
+  public T get(final int index) {
+    return tempTable[index];
+  }
+
+  public T set(final int index, final T element) {
+    tempTable[index] = element;
+    return element;
+  }
+
+  public int indexOf(final Object o) {
+    return 0;
+  }
+
+  public int lastIndexOf(final Object o) {
+    return 0;
+  }
+
+  public ListIterator<T> listIterator() {
+    return null;
+  }
+
+  public ListIterator<T> listIterator(final int index) {
+    return null;
+  }
+
+  public List<T> subList(final int fromIndex, final int toIndex) {
+    if (fromIndex > toIndex) {
+      throw new IllegalArgumentException("fromIndex > toIndex");
+    }
+    if (fromIndex < 0) {
+      throw new IndexOutOfBoundsException("fromIndex = " + fromIndex);
+    }
+    if (toIndex > size) {
+      throw new IndexOutOfBoundsException("toIndex = " + toIndex);
+    }
+    if (fromIndex == toIndex) {
+      return null;
     }
 
-    public boolean remove(final Object o) {
+    T[] table = (T[]) new Object[toIndex - fromIndex];
+
+    for (int i = fromIndex; i < toIndex; i++) {
+      table[i - fromIndex] = tempTable[i];
+    }
+    return new MyArrayList<T>(Arrays.asList(table));
+  }
+
+  public Spliterator<T> spliterator() {
+    return null;
+  }
+
+  @Override
+  public int hashCode() {
+    return 17 * tempTable.hashCode() + 31 * size;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof MyArrayList)) {
+      return false;
+    }
+    if (obj == this) {
+      return true;
+    }
+    MyArrayList<T> other = (MyArrayList<T>) obj;
+    if (other.size() != tempTable.length) {
+      return false;
+    }
+    for (int i = 0; i < other.size(); i++) {
+      if (!other.get(i).equals(tempTable[i])) {
         return false;
+      }
     }
+    return true;
+  }
 
-    public boolean containsAll(final Collection<?> c) {
-        return false;
-    }
-
-    public boolean addAll(final Collection<? extends T> c) {
-        return false;
-    }
-
-    public boolean addAll(final int index, final Collection<? extends T> c) {
-        return false;
-    }
-
-    public boolean removeAll(final Collection<?> c) {
-        return false;
-
-    }
-
-    public boolean retainAll(final Collection<?> c) {
-        return false;
-    }
-
-    public void replaceAll(final UnaryOperator<T> operator) {
-
-    }
-
-    public void sort(final Comparator<? super T> c) {
-
-    }
-
-    public void clear() {
-        tempTable = (T[]) new Object[0];
-        size = 0;
-    }
-
-    public T get(final int index) {
-        return tempTable[index];
-    }
-
-    public T set(final int index, final T element) {
-        tempTable[index]=element;
-        return element;
-    }
-
-    public void add(final int index, final T element) {
-        Object[] createTable = new Object[size+1];
-        System.arraycopy(tempTable, 0, createTable, 0, index);
-        createTable[index] = element;
-        System.arraycopy(tempTable, index, createTable, index+1, size-index);
-        size++;
-        tempTable = (T[])createTable;
-    }
-
-    public T remove(final int index) {
-        return null;
-    }
-
-    public int indexOf(final Object o) {
-        return 0;
-    }
-
-    public int lastIndexOf(final Object o) {
-        return 0;
-    }
-
-    public ListIterator<T> listIterator() {
-        return null;
-    }
-
-    public ListIterator<T> listIterator(final int index) {
-        return null;
-    }
-
-    public List<T> subList(final int fromIndex, final int toIndex) {
-        if (fromIndex>toIndex){
-            throw new IllegalArgumentException("fromIndex > toIndex");
-        }
-        if (fromIndex < 0)
-            throw new IndexOutOfBoundsException("fromIndex = " + fromIndex);
-        if (toIndex > size)
-            throw new IndexOutOfBoundsException("toIndex = " + toIndex);
-        if(fromIndex==toIndex){
-            return null;
-        }
-
-        T[] table = (T[]) new Object[toIndex-fromIndex];
-
-        for(int i=fromIndex; i<toIndex; i++){
-           table[i-fromIndex]=tempTable[i];
-        }
-        return new MyArrayList<T>(Arrays.asList(table));
-    }
-
-    public Spliterator<T> spliterator() {
-        return null;
-    }
-
-    @Override
-    public int hashCode() {
-        return 17 * tempTable.hashCode() + 31 * size;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (obj == null) return false;
-        if (!(obj instanceof MyArrayList)){
-            return false;
-        }
-        if (obj == this){
-            return true;
-        }
-        MyArrayList<T> other = (MyArrayList<T>)obj;
-        if(other.size()!=tempTable.length){
-            return false;
-        }
-        for (int i=0; i<other.size(); i++){
-            if(!other.get(i).equals(tempTable[i])){
-                return false;
-            }
-        }
-        return true;
-    }
 }
--- src/test/java/MyArrayListTest.java
+++ src/test/java/MyArrayListTest.java
@@ -1,130 +1,132 @@
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
-import java.util.List;
-
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class MyArrayListTest {
 
-    @Test
-    void test_size() {
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        assertEquals(table.length, myArray.size());
-    }
+  @Test
+  void test_size() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    assertEquals(table.length, myArray.size());
+  }
 
-    @Test
-    void test_isEmpty_empty_array(){
-        MyArrayList<String> myArray = new MyArrayList<String>();
-        assertTrue(myArray.isEmpty());
-    }
+  @Test
+  void test_isEmpty_empty_array() {
+    MyArrayList<String> myArray = new MyArrayList<String>();
+    assertTrue(myArray.isEmpty());
+  }
 
-    @Test
-    void test_isEmpty_not_empty_array(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        assertFalse(myArray.isEmpty());
-    }
+  @Test
+  void test_isEmpty_not_empty_array() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    assertFalse(myArray.isEmpty());
+  }
 
-    @Test
-    void test_contains_not_contain_object(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        assertFalse(myArray.contains("test"));
-    }
+  @Test
+  void test_contains_not_contain_object() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    assertFalse(myArray.contains("test"));
+  }
 
-    @Test
-    void test_contains_method_contain_object(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        assertTrue(myArray.contains("dwa"));
-    }
-    @Test
-    void test_get_method(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        assertEquals("dwa", myArray.get(1));
-    }
+  @Test
+  void test_contains_method_contain_object() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    assertTrue(myArray.contains("dwa"));
+  }
 
-    @Test
-    void test_add_method_with_id_and_object_params(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        myArray.add(1, "add");
-        assertEquals("add", myArray.get(1));
-    }
+  @Test
+  void test_get_method() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    assertEquals("dwa", myArray.get(1));
+  }
 
-    @Test
-    void test_add_method_with_object_param(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        int index = myArray.size();
-        myArray.add("add");
-        assertEquals("add", myArray.get(index));
-    }
-    @Test
-    void test_sublist_fromIndex_and_toIndex_has_the_same_value(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        assertNull(myArray.subList(1, 1));
-    }
+  @Test
+  void test_add_method_with_id_and_object_params() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    myArray.add(1, "add");
+    assertEquals("add", myArray.get(1));
+  }
 
-    @Test
-    void test_sublist_fromIndex_bigger_than_toIndex(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        try {
-            myArray.subList(2, 1);
-        }
-        catch (IllegalArgumentException e) {
-            assertEquals("fromIndex > toIndex", e.getMessage());
-        }
-    }
+  @Test
+  void test_add_method_with_object_param() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    int index = myArray.size();
+    myArray.add("add");
+    assertEquals("add", myArray.get(index));
+  }
 
-    @Test
-    void test_sublist_fromIndex_less_than_zero(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        try {
-            myArray.subList(-8, 1);
-        }
-        catch (IndexOutOfBoundsException e) {
-            assertEquals("fromIndex = -8", e.getMessage());
-        }
-    }
+  @Test
+  void test_sublist_fromIndex_and_toIndex_has_the_same_value() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    assertNull(myArray.subList(1, 1));
+  }
 
-    @Test
-    void test_sublist_toIndex_bigger_than_size(){
-        String[] table ={"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        try {
-            myArray.subList(1, 15);
-        }
-        catch (IndexOutOfBoundsException e) {
-            assertEquals("toIndex = 15", e.getMessage());
-        }
+  @Test
+  void test_sublist_fromIndex_bigger_than_toIndex() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    try {
+      myArray.subList(2, 1);
+    } catch (IllegalArgumentException e) {
+      assertEquals("fromIndex > toIndex", e.getMessage());
     }
+  }
 
-    @Test
-    void test_sublist_fromIndex_less_than_toIndex() {
-        String[] table = {"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        String[] subTable = {"dwa", "trzy"};
-        MyArrayList<String> subMyArray = new MyArrayList<String>(Arrays.asList(subTable));
-        assertEquals(subMyArray, myArray.subList(1, 3));
+  @Test
+  void test_sublist_fromIndex_less_than_zero() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    try {
+      myArray.subList(-8, 1);
+    } catch (IndexOutOfBoundsException e) {
+      assertEquals("fromIndex = -8", e.getMessage());
     }
+  }
 
-    @Test
-    void test_equals() {
-        String[] table = {"raz", "dwa", "trzy"};
-        MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
-        MyArrayList<String> myArray1 = new MyArrayList<String>(Arrays.asList(table));
-        assertEquals(myArray, myArray1);
-        assertEquals(myArray1, myArray);
-        myArray1.add("cztery");
-        assertNotEquals(myArray, myArray1);
-        assertNotEquals(myArray1, myArray);
+  @Test
+  void test_sublist_toIndex_bigger_than_size() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    try {
+      myArray.subList(1, 15);
+    } catch (IndexOutOfBoundsException e) {
+      assertEquals("toIndex = 15", e.getMessage());
     }
+  }
+
+  @Test
+  void test_sublist_fromIndex_less_than_toIndex() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    String[] subTable = {"dwa", "trzy"};
+    MyArrayList<String> subMyArray = new MyArrayList<String>(Arrays.asList(subTable));
+    assertEquals(subMyArray, myArray.subList(1, 3));
+  }
+
+  @Test
+  void test_equals() {
+    String[] table = {"raz", "dwa", "trzy"};
+    MyArrayList<String> myArray = new MyArrayList<String>(Arrays.asList(table));
+    MyArrayList<String> myArray1 = new MyArrayList<String>(Arrays.asList(table));
+    assertEquals(myArray, myArray1);
+    assertEquals(myArray1, myArray);
+    myArray1.add("cztery");
+    assertNotEquals(myArray, myArray1);
+    assertNotEquals(myArray1, myArray);
+  }
+
 }
